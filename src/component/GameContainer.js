01e040e17a3782efcc160feed1475c03a7085224
tmxhsk99 <tmxhsk99@naver.com>
2023-07-01 14:50:21 +0900

    feat : 배틀관련 css 추가

--- src/component/GameContainer.js
+++ src/component/GameContainer.js
@@ -2,6 +2,12 @@ import "./GameContainer.css"
 import "../module/game/style/global.css"
 import "../module/game/style/TextMessage.css"
 import "../module/game/style/SceneTransition.css"
+
+
+import "../module/game/style/Battle.css"
+import "../module/game/style/Combatant.css"
+import "../module/game/style/SubmissionMenu.css"
+import "../module/game/style/Team.css"
 import React, {useEffect} from "react";
 import {useRef} from "react";
 
